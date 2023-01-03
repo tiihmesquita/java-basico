1eadb260009b0357b48db12ed02226182bb0f3b0
tiihmesquita <thiagomesquita17@gmail.com>
2023-01-03 10:27:25 -0300

    calculadora para somar

---
+++ tipos-variaveis/src/conteudo/javadoc/calculadora.java
@@ -0,0 +1,14 @@
+package conteudo.javadoc;
+
+import java.util.SortedMap;
+
+public class calculadora {
+    public static void main(String[] args) {
+        int num1=2, num2=3;
+        System.out.println(somar (num1,num2));
+    }
+
+    public static int somar (int num1, int num2){
+        return num1 + num2;
+    }
+}
