62579641ee6083e3c0815599a0eb0744d1c1e37c
tiihmesquita <thiagomesquita17@gmail.com>
2023-01-04 09:52:25 -0300

    ultilizando for

---
+++ tipos-variaveis/src/conteudo/exemploFor/ContantoCarneirinho.java
@@ -0,0 +1,10 @@
+package conteudo.exemploFor;
+
+public class ContantoCarneirinho {
+    public static void main(String[] args) {
+        for(int carneirinho = 1; carneirinho <=20; carneirinho++){
+            System.out.println(carneirinho);
+
+        }
+    }
+}
