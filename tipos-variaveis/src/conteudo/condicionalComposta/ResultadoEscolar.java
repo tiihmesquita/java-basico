ee636af64e1735441c6199717726b7aa22b3be60
tiihmesquita <thiagomesquita17@gmail.com>
2023-01-04 09:45:06 -0300

    sistema de medidas

--- tipos-variaveis/src/conteudo/condicionalComposta/ResultadoEscolar.java
+++ tipos-variaveis/src/conteudo/condicionalComposta/ResultadoEscolar.java
@@ -2,13 +2,21 @@ package conteudo.condicionalComposta;
 
 public class ResultadoEscolar {
     public static void main(String[] args) {
-        int nota = 8;
+        int nota = 6;
+        /*
         if(nota >= 7){
             System.out.println("Aprovado");
 
         }
+        else if(nota >= 5 && nota < 7){
+            System.out.println("Prova recuperação");
+        }
         else{
             System.out.println("Reprovado");
         }
+        */
+        String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+        System.out.println(resultado);
+
     }
 }
