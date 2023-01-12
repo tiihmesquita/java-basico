f88c2dddf540543f557931a4a68bd3325600fd4f
tiihmesquita <thiagomesquita17@gmail.com>
2023-01-12 01:46:55 -0300

    refatorando classe no para classe generics

--- tipos-variaveis/src/projeto/estruturaDeDados/projetono/Main.java
+++ tipos-variaveis/src/projeto/estruturaDeDados/projetono/Main.java
@@ -2,12 +2,12 @@ package projeto.estruturaDeDados.projetono;
 
 public class Main {
     public static void main(String[] args) {
-        No no1 = new No("conteudo no1");
-        No no2 = new No("conteudo no2");
+        No<String> no1 = new No<String>("conteudo no1");
+        No<String> no2 = new No<String>("conteudo no2");
         no1.setProximoNo(no2);
-        No no3 = new No("conteudo no3");
+        No<String> no3 = new No<>("conteudo no3");
         no2.setProximoNo(no3);
-        No no4 = new No("conteudo no4");
+        No<String> no4 = new No<>("conteudo no4");
         no3.setProximoNo(no4);
         System.out.println(no1);
         System.out.println(no1.getProximoNo());
