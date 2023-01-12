d936cb96793b3b1942d4bc8f5116025071f94b12
tiihmesquita <thiagomesquita17@gmail.com>
2023-01-12 12:58:24 -0300

    aplicando pilha

---
+++ tipos-variaveis/src/projeto/estruturaDeDados/pilha/Pilha.java
@@ -0,0 +1,35 @@
+package projeto.estruturaDeDados.pilha;
+
+public class Pilha {
+    private No refNoEntradaPilha;
+
+    public Pilha() {
+        this.refNoEntradaPilha = null;
+    }
+
+    public void push(No novoNo){
+        No refAuxiliar = refNoEntradaPilha;
+        refNoEntradaPilha = novoNo;
+        refNoEntradaPilha.setRefNo(refAuxiliar);
+    }
+
+    public No pop(){
+        if(!this.isEmpty()){
+            No noPoped = refNoEntradaPilha;
+            refNoEntradaPilha = refNoEntradaPilha.getRefNo();
+            return noPoped;
+        }
+        return null;
+    }
+
+    public No top(){
+        return refNoEntradaPilha;
+    }
+
+
+    public boolean isEmpty(){
+        
+        return refNoEntradaPilha ==null ? true : false;
+    }
+    
+}
