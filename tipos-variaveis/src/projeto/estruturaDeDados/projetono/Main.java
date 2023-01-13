80c9eb401b1810624359633e5e9fa18872052bb9
tiihmesquita <thiagomesquita17@gmail.com>
2023-01-13 00:07:58 -0300

    validando ultimo no

--- tipos-variaveis/src/projeto/estruturaDeDados/projetono/Main.java
+++ tipos-variaveis/src/projeto/estruturaDeDados/projetono/Main.java
@@ -16,5 +16,6 @@ public class Main {
         System.out.println(no1.getProximoNo());
         System.out.println(no1.getProximoNo().getProximoNo());
         System.out.println(no1.getProximoNo().getProximoNo().getProximoNo());
+        System.out.println(no1.getProximoNo().getProximoNo().getProximoNo().getProximoNo());
     }
 }
