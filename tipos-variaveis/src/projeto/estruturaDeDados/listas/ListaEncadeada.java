691d3863ea4f6c22cf677abac5939b803ad242cd
tiihmesquita <thiagomesquita17@gmail.com>
2023-01-16 11:42:57 -0300

    criando metodo adicionar na lista encadeada

--- tipos-variaveis/src/projeto/estruturaDeDados/listas/ListaEncadeada.java
+++ tipos-variaveis/src/projeto/estruturaDeDados/listas/ListaEncadeada.java
@@ -8,6 +8,25 @@ public class ListaEncadeada<T> {
         this.referenciaEntrada = null;
     }
 
+
+
+    public void add(T conteudo){
+        No<T> novoNo = new No<>(conteudo);
+        if(this.isEmpty()){
+            referenciaEntrada = novoNo;
+            return;
+        }
+
+        No<T> noAuxiliar = referenciaEntrada;
+        for(int i = 0; i < this.size()-1;i++){
+            noAuxiliar = noAuxiliar.getProximoNo();
+
+        }
+        noAuxiliar = noAuxiliar.getProximoNo();
+
+
+    }
+
     public int size(){
         int tamanhoLista=0;
         No<T> referenciaAux = referenciaEntrada;
