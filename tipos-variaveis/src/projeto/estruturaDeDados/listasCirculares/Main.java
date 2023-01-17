29d2d29ecd619b83f075c09df95ed21e65aaf30b
tiihmesquita <thiagomesquita17@gmail.com>
2023-01-17 03:56:59 -0300

    ultilizando o metodo lista circular na main

---
+++ tipos-variaveis/src/projeto/estruturaDeDados/listasCirculares/Main.java
@@ -0,0 +1,21 @@
+package projeto.estruturaDeDados.listasCirculares;
+
+public class Main {
+    public static void main(String[] args) {
+        ListaCircular<String> minhaLista = new ListaCircular<>();
+        
+        minhaLista.add("c0");
+        System.out.println(minhaLista);
+        minhaLista.remove(0);
+        System.out.println(minhaLista);
+        minhaLista.add("c1");
+        System.out.println(minhaLista);
+        minhaLista.add("c2");
+        minhaLista.add("c3");
+        System.out.println(minhaLista);
+        System.out.println(minhaLista.get(8));
+
+    
+    
+    }
+}
