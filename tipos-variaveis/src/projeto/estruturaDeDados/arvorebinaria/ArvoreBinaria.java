0c477f9d52078e5e4c8f399113b947c2a04bee9d
tiihmesquita <thiagomesquita17@gmail.com>
2023-01-19 10:24:38 -0300

    criando metodo de exibição para arvore binaria

--- tipos-variaveis/src/projeto/estruturaDeDados/arvorebinaria/ArvoreBinaria.java
+++ tipos-variaveis/src/projeto/estruturaDeDados/arvorebinaria/ArvoreBinaria.java
@@ -28,6 +28,43 @@ public class ArvoreBinaria<T extends Comparable<T>> {
         }
 
     }
+    public void exibirInOrdem(){
+        System.out.println("\n Exibindo InOrdem");
+        exibirInOrdem(this.raiz);
+    }
+    private void exibirInOrdem(BinNo<T> atual){
+        if(atual != null){
+            exibirInOrdem(atual.getNoEsq());
+            System.out.println(atual.getConteudo()+",");
+            exibirInOrdem(atual.getNoDir());
+        }
+    }
+
+    public void exibirPosOrdem(){
+        System.out.println("\n Exibindo InOrdem");
+        exibirInOrdem(this.raiz);
+    }
+    private void exibirPosOrdem(BinNo<T> atual){
+        if(atual != null){
+            exibirPosOrdem(atual.getNoEsq());
+            exibirPosOrdem(atual.getNoDir());
+            System.out.println(atual.getConteudo()+",");
+            
+        }
+    }
+    public void exibirPreOrdem(){
+        System.out.println("\n Exibindo InOrdem");
+        exibirInOrdem(this.raiz);
+    }
+    private void exibirPreOrdem(BinNo<T> atual){
+        if(atual != null){
+
+            System.out.println(atual.getConteudo()+",");
+            exibirPreOrdem(atual.getNoEsq());
+            exibirPreOrdem(atual.getNoDir());
+                
+        }
+    }
 
 
 }
