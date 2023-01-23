4d3cf1fa7cdce00bfc56d59fbae83734ec905f0b
tiihmesquita <thiagomesquita17@gmail.com>
2023-01-23 01:35:14 -0300

    ordenação de lista final

--- tipos-variaveis/src/collections/list/ExemploOrdenacaoList.java
+++ tipos-variaveis/src/collections/list/ExemploOrdenacaoList.java
@@ -2,6 +2,7 @@ package collections.list;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class ExemploOrdenacaoList {
@@ -19,7 +20,18 @@ public class ExemploOrdenacaoList {
         System.out.println("Ordem Natural: ");
         Collections.sort(meusGatos);
         System.out.println(meusGatos);
-        
+
+        System.out.println("Ordem Idade");
+
+        Collections.sort(meusGatos, new ComparatorIdade());
+        //meusGatos.sort(new ComparatorIdade());
+        System.out.println(meusGatos);
+        System.out.println("Ordem cor");
+        meusGatos.sort(new ComparatorCor());
+        System.out.println(meusGatos);
+        System.out.println("Ordem Nome/Cor/Idade");
+        meusGatos.sort(new ComparatorNomeCorIdade());
+        System.out.println(meusGatos);
     }
 
 }
@@ -58,4 +70,38 @@ class Gato implements Comparable<Gato>{
         return this.getNome().compareToIgnoreCase(o.getNome());
     }
     
+}
+
+class ComparatorIdade implements Comparator<Gato>{
+
+    @Override
+    public int compare(Gato o1, Gato o2) {
+        
+        return Integer.compare(o1.getIdade(), o2.getIdade());
+    }
+
+}
+class ComparatorCor implements Comparator<Gato>{
+
+    @Override
+    public int compare(Gato o1, Gato o2) {
+       
+        return o1.getCor().compareToIgnoreCase(o2.getCor());
+    }
+
+
+}
+class ComparatorNomeCorIdade implements Comparator<Gato>{
+
+   
+
+    @Override
+    public int compare(Gato o1, Gato o2) {
+       int nome = o1.getNome().compareToIgnoreCase(o2.getNome());
+       if(nome != 0)  return nome;
+       int cor = o1.getCor().compareToIgnoreCase(o2.getCor());
+       if(cor != 0) return cor;
+       return Integer.compare(o1.getIdade(),o2.getIdade());
+    }
+
 }
