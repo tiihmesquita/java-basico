78847839f2387d5bbc461c492b8e3cdea409f149
tiihmesquita <thiagomesquita17@gmail.com>
2023-01-24 01:21:12 -0300

    conhecendo metodos Set

--- tipos-variaveis/src/collections/Set/ExemploSet.java
+++ tipos-variaveis/src/collections/Set/ExemploSet.java
@@ -5,7 +5,9 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class ExemploSet {
     public static void main(String[] args) {
@@ -13,7 +15,7 @@ public class ExemploSet {
 
         System.out.println("Crie um conjunto e adicione as notas : ");
     
-        Set<Double> notas = new HashSet<>(Arrays.asList(7d,8.5,9.3,5d,7d,8d,3.6));
+        Set<Double> notas = new HashSet<>(Arrays.asList(0d,7d,8.5,9.3,5d,7d,8d,3.6));
     
         System.out.println(notas.toString());
         System.out.println("Confira se a nota 5.0 está no conjunto: ");
@@ -29,7 +31,30 @@ public class ExemploSet {
          }
          System.out.println("Exiba a soma dos valores: "+soma);
          System.out.println("Exiba a media das notas: "+ (soma/notas.size()));
+         notas.remove(0d);
+         System.out.println("Remova as notas menores que 7 e exiba a lista: ");
+         Iterator<Double> iterator2 = notas.iterator();
+         while(iterator2.hasNext()){
+            Double next = iterator2.next();
+            if(next < 7) iterator2.remove();
+         }
+         System.out.println(notas);
 
+         System.out.println("Exiba todas as notas na ordem em que foram informadas: ");
+         Set<Double> notas2 = new LinkedHashSet<>();
+         notas2.add(2d);
+         notas2.add(5d);
+         notas2.add(6d);
+         notas2.add(3.5);
+         notas2.add(7.5);
+         notas2.add(8d);
+         notas2.add(9.5d);
 
+         System.out.println(notas2);
+         System.out.println("Exiba todas as notas na ordem crescente: ");
+         Set<Double> notas3 = new TreeSet<>(notas2);
+         System.out.println(notas3);
+         notas.clear();
+         System.out.println(notas.isEmpty());
     }
 }
