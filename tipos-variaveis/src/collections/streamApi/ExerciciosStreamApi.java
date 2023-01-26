d9c3c8f6cabc94c95d265ea45d5bf28856b37cb3
tiihmesquita <thiagomesquita17@gmail.com>
2023-01-26 02:31:21 -0300

    finalizando metodos Strem api

--- tipos-variaveis/src/collections/streamApi/ExerciciosStreamApi.java
+++ tipos-variaveis/src/collections/streamApi/ExerciciosStreamApi.java
@@ -4,6 +4,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.ToIntFunction;
 import java.util.stream.Collectors;
 
 public class ExerciciosStreamApi {
@@ -20,8 +22,16 @@ public class ExerciciosStreamApi {
         System.out.println("Transforme essa lista de string em uma lista de numeros inteiros: ");
         List<Integer> collectList = numerosAleatorios.stream().map(Integer::parseInt).collect(Collectors.toList());
         
+        System.out.println("Pegue os numeros pares e maiores que 2 e coloque em uma lista:");
+        List<Integer> listParesMaioresQue2 = numerosAleatorios.stream().map(Integer::parseInt).filter(t -> t % 2 == 0 && t > 2).collect(Collectors.toList());
+        System.out.println(listParesMaioresQue2);
             
-            
-        
+        System.out.println("Mostre a media dos numeros:");
+
+        numerosAleatorios.stream().mapToInt(Integer::parseInt).average().ifPresent(System.out::println);
+
+        System.out.println("Remova os valores impares: ");
+        collectList.removeIf(integer -> (integer % 2!= 0));
+        System.out.println(collectList);
         }
 }
