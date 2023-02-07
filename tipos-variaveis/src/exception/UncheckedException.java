4b9720fef1d50fb93dba8be9b281d6e458965fd9
tiihmesquita <thiagomesquita17@gmail.com>
2023-02-07 02:08:45 -0300

    criando classe unchecked exception

---
+++ tipos-variaveis/src/exception/UncheckedException.java
@@ -0,0 +1,16 @@
+package exception;
+
+import javax.swing.JOptionPane;
+
+public class UncheckedException {
+    public static void main(String[] args) {
+        String a = JOptionPane.showInputDialog("Numerador: ");
+        String b = JOptionPane.showInputDialog("Denominador: ");
+
+        int resultado = dividir(Integer.parseInt(a),Integer.parseInt(b));
+        System.out.println("Resultado = "+resultado);
+    }
+    public static int dividir(int a, int b){
+        return a/b;
+    }
+}
