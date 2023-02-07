90c6c5754adca4f6ea6cd04fc4634fb2bbdf61d2
tiihmesquita <thiagomesquita17@gmail.com>
2023-02-07 02:22:36 -0300

    tratando exceçoes

--- tipos-variaveis/src/exception/UncheckedException.java
+++ tipos-variaveis/src/exception/UncheckedException.java
@@ -4,11 +4,29 @@ import javax.swing.JOptionPane;
 
 public class UncheckedException {
     public static void main(String[] args) {
-        String a = JOptionPane.showInputDialog("Numerador: ");
-        String b = JOptionPane.showInputDialog("Denominador: ");
-
-        int resultado = dividir(Integer.parseInt(a),Integer.parseInt(b));
-        System.out.println("Resultado = "+resultado);
+        boolean continueLooping = true;
+        do{
+            String a = JOptionPane.showInputDialog("Numerador: ");
+            String b = JOptionPane.showInputDialog("Denominador: ");
+            try{
+                int resultado = dividir(Integer.parseInt(a),Integer.parseInt(b));
+                System.out.println("Resultado = "+resultado);
+                continueLooping = false;
+            }catch(NumberFormatException e){
+                JOptionPane.showMessageDialog(null,"Entrada invalida, informe um numero inteiro");
+            }catch(ArithmeticException e){
+                JOptionPane.showMessageDialog(null,"Impossivel dividir um numero por zero");
+            }
+            finally{
+                System.out.println("Chegou no finaly");
+            }
+        }while(continueLooping);
+        
+        
+        
+        System.out.println("O codigo continua ...");
+    
+    
     }
     public static int dividir(int a, int b){
         return a/b;
